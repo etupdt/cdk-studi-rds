ece97796ac4132ba7bfab1339cc8fadc51798d39
etupdt <denis.tavernier@gmail.com>
2023-04-15 19:17:58 +0200

    Initial commit

---
+++ test/cdk-studi-rds.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStudiRds from '../lib/cdk-studi-rds-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-studi-rds-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStudiRds.CdkStudiRdsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
